ab5c754b00d9447128ce7bdcea74054372c8ccd7
MatheusLOliveira <matheuslimaoliveira@gmail.com>
2024-09-25 00:40:17 -0300

    feat: first commit

---
+++ src/main/java/com/maranata/visitor_registration/VisitorRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.maranata.visitor_registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisitorRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisitorRegistrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maranata/visitor_registration/VisitorRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maranata.visitor_registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisitorRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
