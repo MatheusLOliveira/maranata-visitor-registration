6bcd13e240c70103f9456fb2455509b0049f6a91
MatheusLOliveira <matheuslimaoliveira@gmail.com>
2026-01-27 13:04:37 -0300

    feat: Add authorization filter chain

--- src/main/java/com/maranata/xenia/infra/security/SecurityConfiguration.java
+++ src/main/java/com/maranata/xenia/infra/security/SecurityConfiguration.java
@@ -2,11 +2,29 @@ package com.maranata.xenia.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
+
+    // TODO: Fix all methods
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "/api/volunteer").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
