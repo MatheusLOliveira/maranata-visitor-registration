e2a6106b0bb5b74a80bcfdb1f78726de50aae773
MatheusLOliveira <matheuslimaoliveira@gmail.com>
2026-04-21 23:54:36 -0300

    fix: VolunteerController delete method to not return and add Pageable to volunteers

--- src/main/java/com/maranata/xenia/controller/VolunteerController.java
+++ src/main/java/com/maranata/xenia/controller/VolunteerController.java
@@ -5,6 +5,7 @@ import com.maranata.xenia.model.VolunteerRequestDTO;
 import com.maranata.xenia.model.VolunteerResponseDTO;
 import com.maranata.xenia.service.VolunteerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,29 +20,31 @@ public class VolunteerController {
     private VolunteerService service;
 
     @PostMapping
-    public ResponseEntity<VolunteerResponseDTO> create(@RequestBody VolunteerRequestDTO body) {
+    public ResponseEntity<VolunteerResponseDTO> createVolunteer(@RequestBody VolunteerRequestDTO body) {
         VolunteerResponseDTO newVolunteer = this.service.createVolunteer(body);
 
         return ResponseEntity.ok(newVolunteer);
     }
 
-    @GetMapping("/{volunteerName}")
-    public ResponseEntity<VolunteerResponseDTO> getVolunteer(@PathVariable String volunteerName) {
-        VolunteerResponseDTO volunteer = this.service.getVolunteerByName(volunteerName);
+    @GetMapping("/{id}")
+    public ResponseEntity<VolunteerResponseDTO> getVolunteerById(@PathVariable UUID id) {
+        VolunteerResponseDTO volunteer = this.service.getVolunteerById(id);
 
         return ResponseEntity.ok(volunteer);
     }
 
     @GetMapping
-    public ResponseEntity<List<VolunteerResponseDTO>> getVolunteers(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size) {
-        List<VolunteerResponseDTO> volunteersList = this.service.getVolunteers(page, size);
+    public ResponseEntity<Page<VolunteerResponseDTO>> getVolunteers(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
 
+        Page<VolunteerResponseDTO> volunteersList = this.service.getVolunteers(page, size);
         return ResponseEntity.ok(volunteersList);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<VolunteerResponseDTO> deleteVolunteer(@PathVariable UUID id) {
-        this.service.deleteVolunteerById(id);
+        VolunteerResponseDTO deletedVolunteer = this.service.deleteVolunteerById(id);
 
         return ResponseEntity.noContent().build();
     }
